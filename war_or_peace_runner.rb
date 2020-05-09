3f5b6f7cf30207bfe056a70d73db26e8874499fc
Dan Sehl <dan.sehl@gmail.com>
2020-05-08 21:47:19 -0600

    Write runner file

--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -2,3 +2,8 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
+require './lib/game_setup'
+
+puts "Currently there is a problem with the winner method in turn, fix that first"
+game = GameSetUp.new
+game.start
