2cf004fa83e88e2302083a5e0053feae7dbb36ed
Dan Sehl <dan.sehl@gmail.com>
2020-05-08 18:24:20 -0600

    Write functionality to start the game and test this action

--- lib/game_setup.rb
+++ lib/game_setup.rb
@@ -1,13 +1,17 @@
 class GameSetUp
 
   attr_reader :deck,
-              :deck_1
-              :deck_2
+              :deck_1,
+              :deck_2,
               :suits,
               :values,
-              :ranks
-              :player_1
-              :player_2
+              :ranks,
+              :player1,
+              :player2,
+              :start_string_1,
+              :start_string_2
+              :starting_pistol
+
   def initialize
     @deck = []
     @suits = %i[heart diamond spade club]
@@ -15,7 +19,7 @@ class GameSetUp
     @ranks = (2..14).to_a
   end
 
-  def new_decks
+  def new_deck
     @suits.each do |suit|
       @values.count.times do |counter|
         @deck << Card.new(suit, @values[counter], @ranks[counter])
@@ -33,5 +37,23 @@ class GameSetUp
     @player1 = Player.new("Katie", deck_1)
     @player2 = Player.new("Kelly", deck_2)
   end
-  
+
+  def start
+    self.new_deck
+    self.create_two_shuffled_decks
+    self.assign_decks
+    @start_string_1 = "Welcome to War! (or Peace) This game will be played with #{@deck.count} cards."
+    @start_string_2 = "The players today are #{self.player1.name} and #{self.player2.name}."
+    puts @start_string_1
+    puts @start_string_2
+    puts "Type 'GO' to start the game!"
+    puts "------------------------------------------------------------------"
+    @starting_pistol = gets.chomp.upcase
+    if @starting_pistol =! "GO"
+      puts "Please re-type GO and hit enter"
+    else
+      # code/method call to play the game starts here
+
+  end
+
 end
--- test/game_setup_test.rb
+++ test/game_setup_test.rb
@@ -13,10 +13,54 @@ class GameSetUpTest < Minitest::Test
     assert_instance_of GameSetUp, setting_up
   end
 
+  def test_deck_can_exist
+    setting_up = GameSetUp.new
+    setting_up.new_deck
+
+    assert_instance_of Array, setting_up.deck
+  end
+
   def test_deck_has_52_cards
     setting_up = GameSetUp.new
     setting_up.new_deck
+
     assert_equal 52, setting_up.deck.count
+  end
+
+  def test_two_decks_can_exist
+    setting_up = GameSetUp.new
+    setting_up.new_deck
+    setting_up.create_two_shuffled_decks
+
+    assert_instance_of Array, setting_up.deck_1
+    assert_instance_of Array, setting_up.deck_2
+  end
+
+  def test_two_decks_can_exist
+    setting_up = GameSetUp.new
+    setting_up.new_deck
+    setting_up.create_two_shuffled_decks
+
+    assert_equal 26, setting_up.deck_1.count
+    assert_equal 26, setting_up.deck_2.count
+  end
+
+  def test_start_creates_two_decks_and_assigns_them
+    setting_up = GameSetUp.new
+    setting_up.start
+
+    assert_equal 26, setting_up.deck_1.count
+    assert_equal Array, setting_up.player1.deck.class
+    assert_equal 26, setting_up.deck_2.count
+    assert_equal Array, setting_up.player2.deck.class
+  end
+
+  def test_start_outputs_correct_strings_52_cards_Katie_Kelly
+    setting_up = GameSetUp.new
+    setting_up.start
+
+    assert_equal "Welcome to War! (or Peace) This game will be played with 52 cards.", setting_up.start_string_1
+    assert_equal "The players today are Katie and Kelly.", setting_up.start_string_2
   end
 
 end
